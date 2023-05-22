d850d7f0bc715f34b22205acd4957147f84b9195
YanranC <614267739@qq.com>
2023-05-22 02:07:11 +0000

    ADD

--- mydartserver/bin/mydartserver.dart
+++ mydartserver/bin/mydartserver.dart
@@ -1,48 +1,98 @@
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:args/args.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart' as shelf_router;
+import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
-const lineNumber = 'line-number';
+Future<void> main() async {
+  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
+  // https://cloud.google.com/run/docs/reference/container-contract#port
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-void main(List<String> arguments) {
-  exitCode = 0; // presume success
-  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+  // See https://pub.dev/documentation/shelf/latest/shelf/Cascade-class.html
+  final cascade = Cascade()
+      // First, serve files from the 'public' directory
+      .add(_staticHandler)
+      // If a corresponding file is not found, send requests to a `Router`
+      .add(_router);
 
-  ArgResults argResults = parser.parse(arguments);
-  final paths = argResults.rest;
+  // See https://pub.dev/documentation/shelf/latest/shelf_io/serve.html
+  final server = await shelf_io.serve(
+    // See https://pub.dev/documentation/shelf/latest/shelf/logRequests.html
+    logRequests()
+        // See https://pub.dev/documentation/shelf/latest/shelf/MiddlewareExtensions/addHandler.html
+        .addHandler(cascade.handler),
+    InternetAddress.anyIPv4, // Allows external connections
+    port,
+  );
 
-  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+  print('Serving at http://${server.address.host}:${server.port}');
+
+  // Used for tracking uptime of the demo server.
+  _watch.start();
 }
 
-Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
-  if (paths.isEmpty) {
-    // No files provided as arguments. Read from stdin and print each line.
-    await stdin.pipe(stdout);
-  } else {
-    for (final path in paths) {
-      var lineNumber = 1;
-      final lines = utf8.decoder
-          .bind(File(path).openRead())
-          .transform(const LineSplitter());
-      try {
-        await for (final line in lines) {
-          if (showLineNumbers) {
-            stdout.write('${lineNumber++} ');
-          }
-          stdout.writeln(line);
-        }
-      } catch (_) {
-        await _handleError(path);
-      }
-    }
-  }
+// Serve files from the file system.
+final _staticHandler =
+    shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
+
+// Router instance to handler requests.
+final _router = shelf_router.Router()
+  ..get('/helloworld', _helloWorldHandler)
+  ..get(
+    '/time',
+    (request) => Response.ok(DateTime.now().toUtc().toIso8601String()),
+  )
+  ..get('/info.json', _infoHandler)
+  ..get('/sum/<a|[0-9]+>/<b|[0-9]+>', _sumHandler);
+
+Response _helloWorldHandler(Request request) => Response.ok('Hello, World!');
+
+String _jsonEncode(Object? data) =>
+    const JsonEncoder.withIndent(' ').convert(data);
+
+const _jsonHeaders = {
+  'content-type': 'application/json',
+};
+
+Response _sumHandler(Request request, String a, String b) {
+  final aNum = int.parse(a);
+  final bNum = int.parse(b);
+  return Response.ok(
+    _jsonEncode({'a': aNum, 'b': bNum, 'sum': aNum + bNum}),
+    headers: {
+      ..._jsonHeaders,
+      'Cache-Control': 'public, max-age=604800, immutable',
+    },
+  );
 }
 
-Future<void> _handleError(String path) async {
-  if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('error: $path is a directory');
-  } else {
-    exitCode = 2;
-  }
-}
+final _watch = Stopwatch();
+
+int _requestCount = 0;
+
+final _dartVersion = () {
+  final version = Platform.version;
+  return version.substring(0, version.indexOf(' '));
+}();
+
+Response _infoHandler(Request request) => Response(
+      200,
+      headers: {
+        ..._jsonHeaders,
+        'Cache-Control': 'no-store',
+      },
+      body: _jsonEncode(
+        {
+          'Dart version': _dartVersion,
+          'uptime': _watch.elapsed.toString(),
+          'requestCount': ++_requestCount,
+        },
+      ),
+    );
